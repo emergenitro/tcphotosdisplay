104b848196e1f972d97c8f63ba7be3af8d7fbd24
Karthik Sankar <emergenitro@gmail.com>
2024-07-07 22:43:31 +0800

    Initial Commit

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -10,9 +10,9 @@ export async function search(options = {}) {
 
   const paramString = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join('&');
 
-  const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/search?${paramString}`, {
+  const results = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/resources/search?${paramString}`, {
     headers: {
-      Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+      Authorization: `Basic ${Buffer.from(process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY + ':' + process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET).toString('base64')}`
     }
   }).then(r => r.json());
 
@@ -24,20 +24,9 @@ export function mapImageResources(resources) {
     const { width, height } = resource;
     return {
       id: resource.asset_id,
-      title: resource.public_id,
       image: resource.secure_url,
       width,
       height
     }
   });
-}
-
-export async function getFolders(options = {}) {
-  const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/folders`, {
-    headers: {
-      Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
-    }
-  }).then(r => r.json());
-
-  return response;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
 import Button from '@components/Button';
+import Container from '@components/Container';
+import Layout from '@components/Layout';
 
-import { search, mapImageResources, getFolders } from '../lib/cloudinary';
+import { mapImageResources, search } from '../lib/cloudinary';
 
-import styles from '@styles/Home.module.scss'
+import styles from '@styles/Home.module.scss';
 
-export default function Home({ images: defaultImages, nextCursor: defaultNextCursor, totalCount: defaultTotalCount, folders }) {
+export default function Home({ images: defaultImages, nextCursor: defaultNextCursor, totalCount: defaultTotalCount }) {
   const [images, setImages] = useState(defaultImages);
   const [nextCursor, setNextCursor] = useState(defaultNextCursor);
   const [totalCount, setTotalCount] = useState(defaultTotalCount);
   const [activeFolder, setActiveFolder] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
 
   async function handleOnLoadMore(e) {
     e.preventDefault();
@@ -68,46 +70,47 @@ export default function Home({ images: defaultImages, nextCursor: defaultNextCur
     })();
   }, [activeFolder]);
 
+  function openModal(image) {
+    setCurrentImage(image);
+    setIsModalOpen(true);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+
   return (
     <Layout>
       <Head>
-        <title>My Images</title>
+        <title>GIIS Tech Club 2K24</title>
         <meta name="description" content="All of my cool images." />
       </Head>
 
       <Container>
-        <h1 className="sr-only">My Images</h1>
-
-        <h2>Folders</h2>
-
-        <ul className={styles.folders} onClick={handleOnFolderClick}>
-          {folders.map(folder => {
-            const isActive = folder.path === activeFolder;
-            return (
-              <li key={folder.path} data-active-folder={isActive}>
-                <button data-folder-path={folder.path} >{ folder.name }</button>
-              </li>
-            )
-          })}
-        </ul>
+        <h1 className="sr-only">GIIS Hackathon X - 26th and 27th July 2K24</h1>
 
         <h2 className={styles.header}>Images</h2>
 
         <ul className={styles.images}>
-          {images.map(image => {
-            return (
-              <li key={image.id}>
-                <a href={image.link} rel="noreferrer">
-                  <div className={styles.imageImage}>
-                    <Image width={image.width} height={image.height} src={image.image} alt="" />
-                  </div>
-                  <h3 className={styles.imageTitle}>
-                    { image.title }
-                  </h3>
-                </a>
-              </li>
-            )
-          })}
+          {images.map(image => (
+            <li key={image.id}>
+              <a href={image.link} rel="noreferrer" onClick={(e) => {
+                e.preventDefault(); // Prevent the default link behavior
+                openModal(image); // Pass the current image to openModal
+              }}>
+                <div className={styles.imageImage}>
+                  <Image width={image.width} height={image.height} src={image.image} alt="" />
+                </div>
+              </a>
+              {isModalOpen && currentImage && currentImage.id === image.id && (
+                <div id="myModal" className={styles.modal} onClick={closeModal}>
+                  <span className={styles.close}>&times;</span>
+                  <Image src={currentImage.image} className={styles.modalContent} alt="" width={currentImage.width} height={currentImage.height} />
+                </div>
+              )}
+            </li>
+          ))}
         </ul>
         {totalCount > images.length && (
           <p>
@@ -128,14 +131,12 @@ export async function getStaticProps() {
 
   const images = mapImageResources(resources);
 
-  const { folders } = await getFolders();
 
   return {
     props: {
       images,
       nextCursor,
-      totalCount,
-      folders
+      totalCount
     }
   }
 }
